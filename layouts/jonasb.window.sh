6d574968fa119f22a6c479ecd8c0db74c2423cc1
Jonas Bygdén <github@bygden.nu>
2014-04-10 13:53:35 +0200

    My work setup

---
+++ layouts/jonasb.window.sh
@@ -0,0 +1,31 @@
+# Set window root path. Default is `$session_root`.
+# Must be called before `new_window`.
+#window_root "~/Projects/jonasb"
+
+# Create new window. If no argument is given, window name will be based on
+# layout file name.
+new_window "jonasb"
+
+# Split window into panes.
+#split_v 20
+#split_h 50
+
+# Run commands.
+#run_cmd "top"     # runs in active pane
+#run_cmd "date" 1  # runs in pane 1
+
+# Paste text
+#send_keys "top"    # paste into active pane
+#send_keys "date" 1 # paste into active pane
+
+# Set active pane.
+#select_pane 0
+tmuxifier-tmux new-window -t "$session:" -d -n "pe1" "ssh pe1"
+tmuxifier-tmux new-window -t "$session:" -d -n "tc01" "ssh tc01"
+tmuxifier-tmux new-window -t "$session:" -d -n "isyspup1" "ssh isyspup1"
+tmuxifier-tmux new-window -t "$session:" -d -n "psyspup1" "ssh psyspup1"
+tmuxifier-tmux new-window -t "$session:" -d -n "foreman" "ssh foreman"
+tmuxifier-tmux new-window -t "$session:" -d -n "psysmon1" "ssh psysmon1"
+tmuxifier-tmux new-window -t "$session:" -d -n "psysbkp1" "ssh psysbkp1"
+tmuxifier-tmux new-window -t "$session:" -d -n "psysbkp2" "ssh psysbkp2"
+tmuxifier-tmux new-window -t "$session:" -d -n "dsyspup1" "ssh dsyspup1"
